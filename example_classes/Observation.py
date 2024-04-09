96082bb12ddcdb6c336d385d9e81ae515f2d8213
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2024-04-08 22:47:32 -0400

    Add initial Data

---
+++ example_classes/Observation.py
@@ -0,0 +1,10 @@
+class ConversionObservation:
+
+    def __init__(self, bidPrice: float, askPrice: float, transportFees: float, exportTariff: float, importTariff: float, sunlight: float, humidity: float):
+        self.bidPrice = bidPrice
+        self.askPrice = askPrice
+        self.transportFees = transportFees
+        self.exportTariff = exportTariff
+        self.importTariff = importTariff
+        self.sunlight = sunlight
+        self.humidity = humidity
